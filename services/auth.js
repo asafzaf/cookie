e25c114b06f403600295f96950617404d5d71e96
Asafz <asaf.zafrir@gmail.com>
2024-11-28 00:21:34 +0200

    fix

--- services/auth.js
+++ services/auth.js
@@ -1,9 +1,28 @@
 import axios from "axios";
 import { handleAxiosError } from "../http/axios.error";
 
-const cred_file = process.env.CRED_FILE ?? require("../config/creds.json");
+let credFile = {};
 
-const API_KEY = cred_file.firebase_api_key;
+try {
+  credFile = require("../config/creds.json");
+} catch (error) {
+  if (error.code === "MODULE_NOT_FOUND") {
+    console.log("creds.json not found. Using environment variables instead.");
+  } else {
+    console.error("An error occurred while loading creds.json:", error);
+    throw error; // Re-throw unexpected errors
+  }
+}
+
+const data = {
+  cred_file: process.env.CRED_FILE
+    ? JSON.parse(process.env.CRED_FILE)
+    : credFile,
+};
+
+const API_KEY = data.cred_file.firebase_api_key || "default_api_key"; // Optional default value
+
+console.log("API Key:", API_KEY);
 
 export const signUp = async (email, password) => {
   try {
@@ -24,6 +43,7 @@ export const signUp = async (email, password) => {
 };
 
 export const login = async (email, password) => {
+  console.log("API_KEY: ", API_KEY);
   try {
     const response = await axios.post(
       "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" +
